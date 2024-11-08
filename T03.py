7dd84db4d950de3fafb85031303a30519afe6612
prepcb <123950776+prepcb@users.noreply.github.com>
2024-11-08 09:09:36 +0000

    working Gaussian part 1

--- T03.py
+++ T03.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-A = np.array([[1,1,1,1],[0,1,-2,0],[-3,1,0,0]])
+A = np.array([[1,1,1,1],[-3,1,0,0],[0,1,-2,0]])
+
+print(A)
 
 
 def VsV(a,b,n):
     return(b - a*(b[n]/a[n]))
 
 
-print(VsV(A[0,:],A[2,:],0))
 
-#for i in range(2):#
-   # for j in range(i+1,3):
-       # A[j,:] = VsV(A[i,:],A[i+j,:],i)
 
+for i in range(2):
+     for j in range(i+1,3):
+        print(i,j)
+        A[j,:] = VsV(A[i,:],A[j,:],i)
 print(A)
 
-print(np.matmul(np.array([1,2,3]),np.array([[2,3],[3,4],[3,5]])))
+
